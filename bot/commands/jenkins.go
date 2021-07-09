9c51dd500e4f5a2679d87b3e7fbe387e88fd79cc
Eduardo Uliana <edu02_rko@hotmail.com>
2021-07-09 17:59:41 -0300

    Adicionado teste do jenkins

---
+++ bot/commands/jenkins.go
@@ -0,0 +1,13 @@
+package commands
+
+func ExecuteBuildTest() string {
+	return "Executando build teste...."
+}
+
+func ExecuteBuildPDV() string {
+	return "Executando build teste...."
+}
+
+func ExecuteBuildS1() string {
+	return "Executando build teste...."
+}
